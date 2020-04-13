bbcde58240cdbb1663724bd3b671c7c8d01a2d51
Alex Roan <alex.roan@hotmail.com>
2020-04-13 16:22:49 +0100

    Setup Redux and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,62 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
-
+import {connect} from 'react-redux';
 import "./App.css";
+import { loadWeb3, loadContract, loadAccount, loadStoredData } from "./redux/interactions";
+import { contractSelector, accountSelector, valueSelector } from "./redux/selectors";
+import { subscribeToAccountsChanging } from "./redux/subscriptions";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+    const {dispatch, contract, account, value} = this.props;
+
+    const connectBlockchain = async (e) => {
+      e.preventDefault();
+      const myWeb3 = await loadWeb3(dispatch);
+      await loadAccount(dispatch, myWeb3);
+      const myContract = await loadContract(dispatch, myWeb3);
+      await loadStoredData(dispatch, myContract);
+      subscribeToAccountsChanging(dispatch, myWeb3);
     }
+
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="container py-2">
+        <div className="row justify-content-center">
+          <div className="col-4">
+            <form onSubmit={connectBlockchain}>
+              <div className="form-group row">
+                <div className="col-12">
+                  <button type="submit" className={`w-100 btn text-truncate ${(contract !== null) ? "disabled btn-success" : "btn-danger"}`}>
+                    {(contract !== null) ? "Blockchain Connected" : "Connect Blockchain"}
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-4">
+            <label>Account: {account}</label>
+            <p>Changing accounts in Metamask should refresh this account address</p>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-4">
+            <label>Contract Value: </label>
+            <label>{value}</label>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+
+function mapStateToProps(state){
+	return {
+    contract: contractSelector(state),
+    account: accountSelector(state),
+    value: valueSelector(state)
+	}
+}
+
+export default connect(mapStateToProps)(App);
